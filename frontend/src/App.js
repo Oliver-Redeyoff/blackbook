657c9a2580974124b6bd6537a5cadf3cb3fe465a
Oliver Redeyoff <oliver.redeyoff@gmail.com>
2022-12-02 18:10:54 +0000

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div className='header'>
+        <h1 className='header-title'>Creative Book Northumberland</h1>
+        <div className='header-sections'>
+          
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
