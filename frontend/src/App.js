a7168b71ffa62d5934ff8913f6452293151b825f
Oliver Redeyoff <oliver.redeyoff@gmail.com>
2022-12-02 19:11:45 +0000

    Added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+
+  const [section, setSection] = useState(0)
+
   return (
     <div>
 
       <div className='header'>
         <h1 className='header-title'>Creative Book Northumberland</h1>
         <div className='header-sections'>
-          
+          <Button variant={section==0 ? 'contained' : 'text'} onClick={() => {setSection(0)}}>About</Button>
+          <Button variant={section==1 ? 'contained' : 'text'} onClick={() => {setSection(1)}}>Portfolio page</Button>
+          <Button variant={section==2 ? 'contained' : 'text'} onClick={() => {setSection(2)}}>For buisnesses</Button>
+          <Button variant={section==3 ? 'contained' : 'text'} onClick={() => {setSection(3)}}>For creatives</Button>
+          <Button variant={section==4 ? 'contained' : 'text'} onClick={() => {setSection(4)}}>Home</Button>
         </div>
       </div>
       
     </div>
-  );
+  )
+
 }
 
 export default App;
